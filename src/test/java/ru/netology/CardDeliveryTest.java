2c53076c258f2a1a47e4d89a5c0b8fe1b914a5b7
Aleksandr Pershikov <pershikov-ai@mail.ru>
2023-11-05 13:42:24 +0500

    Change CardDeliveryTest 5

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -2,21 +2,20 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 class CardDeliveryTest {
     @BeforeEach
     void SetUp() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
 
@@ -27,16 +26,22 @@ class CardDeliveryTest {
     @Test
     void shouldTest() {
 
+        long days = 29;
+        String planningDate = generateDate(days, "dd.MM.yyyy");
         $("[data-test-id=city] input").setValue("ек");
         $$(".menu-item__control").findBy(Condition.exactText("Екатеринбург")).click();
         $(".input__icon").click();
-        $(".calendar__arrow_direction_right[data-step='1']").click();
-        $$(".calendar__day").findBy(Condition.exactText("4")).click();
+        if (!generateDate(days, "MM").equals(generateDate(0, "MM"))) {
+            $(".calendar__arrow_direction_right[data-step='1']").click();
+            $$(".calendar__day").findBy(Condition.exactText(String.valueOf(LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("d"))))).click();
+        } else {
+            $$(".calendar__day").findBy(Condition.exactText(String.valueOf(LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("d"))))).click();
+        }
         $("[data-test-id=name] input").setValue("Першиков Александр");
         $("[data-test-id=phone] input").setValue("+79222161614");
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $("[data-test-id='notification']").shouldBe(Condition.hidden);
-        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.exactText("Успешно!"), Duration.ofSeconds(11));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(11));
     }
 }
